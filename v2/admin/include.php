6cd09d7d413bb255b73892fd008bad8596eff0f2
ShiinaMinatus <xij1111@gmail.com>
2014-01-14 18:00:06 +0800

    上传admin include 服务器版本配置 Signed-off-by: ShiinaMinatus <xij1111@gmail.com>

--- v2/admin/include.php
+++ v2/admin/include.php
@@ -1,14 +1,18 @@
 <?php
 session_start();
+
+const IPADDRESS='http://112.124.25.155';
+//const IPADDRESS='http://127.0.0.1';
+defined('SERVERADDRESS') or define('SERVERADDRESS', IPADDRESS);      
 defined('FOOT_') or define('FOOT_', $_SERVER['DOCUMENT_ROOT']);
 defined('FOOT') or define('FOOT', FOOT_ . '/008/v2/admin/');
 defined('FOOTBASIC') or define('FOOTBASIC', FOOT_ . '/008/v2/admin/basicClasses/');
 defined('FOOTCLASS') or define('FOOTCLASS', FOOT_ . '/008/v2/admin/classes/');
 defined('FOOTController') or define('FOOTController', FOOT_ . '/008/v2/admin/publicController/');
-defined('URLController') or define('URLController', 'http://127.0.0.1/008/v2/admin/');
-defined('URLAjaxController') or define('URLAjaxController', 'http://127.0.0.1/008/v2/admin/ajax');
-defined('URLJsController') or define('URLJsController', 'http://127.0.0.1/008/v2/admin/js');
-defined('URLAPI') or define('URLAPI', 'http://127.0.0.1/008');
+defined('URLController') or define('URLController', IPADDRESS.'/008/v2/admin/');
+defined('URLAjaxController') or define('URLAjaxController', IPADDRESS.'/008/v2/admin/ajax');
+defined('URLJsController') or define('URLJsController', IPADDRESS.'/008/v2/admin/js');
+defined('URLAPI') or define('URLAPI', IPADDRESS.'/008');
 defined('EXCELREAD') or define('EXCELREAD', FOOT . 'excelfile/');
 require_once FOOT . 'js/smarty.php';
 require_once FOOT . '/plug/reader.php';   //excel 文件问题
